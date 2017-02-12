fb8232a12eb199ceff83622b7127663272363f5f
MattMcMurray <mathieumcmurray@gmail.com>
2017-02-12 11:31:08 -0600

    Modified akaripuzzle to fill cols/rows w/ char of choosing

--- backtrack/AkariPuzzle.java
+++ backtrack/AkariPuzzle.java
@@ -202,19 +202,20 @@ public class AkariPuzzle {
 	}
 
 	public Boolean placeBulbIfPossible(Coordinate initCell) {
+		final char ch = '*';
 		Boolean fillSuccess = true;
 		char[][] gameBoardCopy = deepCopyGameBoard();
 
-		if (!fillColUp(initCell)) {
+		if (!fillColUp(initCell, ch)) {
 			fillSuccess = false;
 		}
-		if (!fillColDown(initCell)) {
+		if (!fillColDown(initCell, ch)) {
 			fillSuccess = false;
 		}
-		if (!fillRowLeft(initCell)) {
+		if (!fillRowLeft(initCell, ch)) {
 			fillSuccess = false;
 		}
-		if (!fillRowRight(initCell)) {
+		if (!fillRowRight(initCell, ch)) {
 			fillSuccess = false;
 		}
 
@@ -228,7 +229,7 @@ public class AkariPuzzle {
 		return fillSuccess;
 	}
 
-	private Boolean fillColUp(Coordinate initCell) {
+	private Boolean fillColUp(Coordinate initCell, char ch) {
 		// Fills cells with '*' to represent light in the upwards direction
 
 		// Returns true if no constraints violated
@@ -242,7 +243,7 @@ public class AkariPuzzle {
 		while (keepGoing) {
 			if (currCell.x >= 0) {
 				if (this.gameBoard[currCell.x][currCell.y] == '_' || this.gameBoard[currCell.x][currCell.y] == '*') {
-					this.gameBoard[currCell.x][currCell.y] = '*';
+					this.gameBoard[currCell.x][currCell.y] = ch;
 					currCell = new Coordinate(currCell.x - OFFSET, currCell.y); // Move curr Cell up by one
 				} else if (Character.isDigit(this.gameBoard[currCell.x][currCell.y])) {
 					keepGoing = false;
@@ -257,7 +258,7 @@ public class AkariPuzzle {
 		return !constraintViolated;
 	}
 
-	private Boolean fillRowRight(Coordinate initCell) {
+	private Boolean fillRowRight(Coordinate initCell, char ch) {
 		// Fills cells with '*' to represent light in the upwards direction
 
 		// Returns true if no constraints violated
@@ -272,7 +273,7 @@ public class AkariPuzzle {
 
 			if (currCell.y < this.cols) {
 				if (this.gameBoard[currCell.x][currCell.y] == '_' || this.gameBoard[currCell.x][currCell.y] == '*') {
-					this.gameBoard[currCell.x][currCell.y] = '*';
+					this.gameBoard[currCell.x][currCell.y] = ch;
 					currCell = new Coordinate(currCell.x, currCell.y + OFFSET); // Move curr Cell up by one
 				} else if (Character.isDigit(this.gameBoard[currCell.x][currCell.y])) {
 					keepGoing = false;
@@ -287,7 +288,7 @@ public class AkariPuzzle {
 		return !constraintViolated;
 	}
 
-	private Boolean fillRowLeft(Coordinate initCell) {
+	private Boolean fillRowLeft(Coordinate initCell, char ch) {
 		// Fills cells with '*' to represent light in the upwards direction
 
 		// Returns true if no constraints violated
@@ -301,7 +302,7 @@ public class AkariPuzzle {
 		while (keepGoing) {
 			if (currCell.y >= 0) {
 				if (this.gameBoard[currCell.x][currCell.y] == '_' || this.gameBoard[currCell.x][currCell.y] == '*') {
-					this.gameBoard[currCell.x][currCell.y] = '*';
+					this.gameBoard[currCell.x][currCell.y] = ch;
 					currCell = new Coordinate(currCell.x, currCell.y - OFFSET); // Move curr Cell up by one
 				} else if (Character.isDigit(this.gameBoard[currCell.x][currCell.y])) {
 					keepGoing = false;
@@ -316,7 +317,7 @@ public class AkariPuzzle {
 		return !constraintViolated;
 	}
 
-	private Boolean fillColDown(Coordinate initCell) {
+	private Boolean fillColDown(Coordinate initCell, char ch) {
 		// Fills cells with '*' to represent light in the upwards direction
 
 		// Returns true if no constraints violated
@@ -330,7 +331,7 @@ public class AkariPuzzle {
 		while (keepGoing) {
 			if (currCell.x < this.cols) {
 				if (this.gameBoard[currCell.x][currCell.y] == '_' || this.gameBoard[currCell.x][currCell.y] == '*') {
-					this.gameBoard[currCell.x][currCell.y] = '*';
+					this.gameBoard[currCell.x][currCell.y] = ch;
 					currCell = new Coordinate(currCell.x + OFFSET, currCell.y); // Move curr Cell up by one
 				} else if (Character.isDigit(this.gameBoard[currCell.x][currCell.y])) {
 					keepGoing = false;
