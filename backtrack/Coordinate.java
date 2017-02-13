26a0a228080bb8e82de20b97d086fea7bdbc0897
MattMcMurray <mathieumcmurray@gmail.com>
2017-02-13 12:02:10 -0600

    Updated Coordinate objects .equals() method to override object

--- backtrack/Coordinate.java
+++ backtrack/Coordinate.java
@@ -1,23 +1,32 @@
 public class Coordinate { 
-    // A little counter-intuitive, but x represents row and y represents column
-    public final int x; 
-    public final int y; 
-    public Coordinate(int row, int col) { 
-        this.x = row; 
-        this.y = col; 
-    }
+	// A little counter-intuitive, but x represents row and y represents column
+	public final int x; 
+	public final int y; 
+	public Coordinate(int row, int col) { 
+		this.x = row; 
+		this.y = col; 
+	}
 
-    public String toString() {
-        return "[" + this.x + ", " + this.y + "]";
-    }
+	public String toString() {
+		return "[" + this.x + ", " + this.y + "]";
+	}
 
-    public Boolean equals(Coordinate coord) {
-        Boolean equal = true;
+	public boolean equals(Object o) {
+		if (!(o instanceof Coordinate)) {
+			return false;
+		}
+		boolean equal = true;
+		Coordinate coord = (Coordinate) o;
 
-        if (this.x != coord.x || this.y != coord.y) {
-            equal = false;
-        }
+		if (this.x != coord.x || this.y != coord.y) {
+			equal = false;
+		}
 
-        return equal;
-    }
+		return equal;
+	}
+
+	public int hashCode() {
+		String hashStr = "" + this.x + this.y;
+		return hashStr.hashCode();
+	}
 }
